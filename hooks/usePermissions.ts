9663d1d25428a6263b66ffac6c19a76e833489a9
Christophe-DC <104490398+Christophe-DC@users.noreply.github.com>
2025-06-14 09:58:16 +0200

    Updated usePermissions.ts

--- hooks/usePermissions.ts
+++ hooks/usePermissions.ts
@@ -202,6 +202,7 @@ export function usePermissions() {
     isLoading,
     requestSinglePermission,
     requestMicrophonePermission,
+    checkMicrophonePermission,
     requestDiagnosticPermissions,
     recheckPermissions,
     hasRequestedPermissions,
