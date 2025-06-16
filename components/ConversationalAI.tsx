f71e965d48604a56c70d7156adb841e7828f0605
Christophe-DC <104490398+Christophe-DC@users.noreply.github.com>
2025-06-16 09:33:12 +0200

    Updated ConversationalAI.tsx

--- components/ConversationalAI.tsx
+++ components/ConversationalAI.tsx
@@ -57,7 +57,7 @@ export default function ConversationalAI({
   onConnect,
   onDisconnect,
   onError,
-    check,
+    requestMicrophonePermission,
     getDeviceInfos,
     updateDiagnosticStep,
     updatePhraseToRead,
